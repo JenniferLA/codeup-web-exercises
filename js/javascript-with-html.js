18496ba267cc763b2fa81dcf774f4a597868c232
Jennifer Park <jennayholla@gmail.com>
2023-09-08 10:25:30 -0500

    reviewed confirm

---
+++ js/javascript-with-html.js
@@ -0,0 +1,17 @@
+// alert("hey!");
+
+"use strict"; 
+
+let myNumber = 7; 
+
+// alert("Hello, I am an alert!");
+
+// let userChoice = confirm("Wait! Don't leave yet! Can I chat with you!");
+
+// console.log(userChoice);
+
+if (userChoice === true){
+    console.log("yay!")
+} else {
+    console.log("boooo");
+} 
